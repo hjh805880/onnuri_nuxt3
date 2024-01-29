08416178aeaedadceeeccd0f02779fba8e45e70b
hjh805880 <hjh805880@gmail.com>
2024-01-29 11:17:05 +0900

    gitignore 업데이트

--- .nuxt/dist/server/_nuxt/entry-styles.SQ_k4OCA.mjs
+++
@@ -1,3 +0,0 @@
-import style_0 from './entry-styles-1.mjs-TCZSiyzk.js';
-import style_1 from './entry-styles-2.mjs-DgoGqKIQ.js';
-export default [style_0, style_1]
--- .nuxt/dist/server/styles.mjs
+++
@@ -1,6 +0,0 @@
-const interopDefault = r => r.default || r || []
-export default {
-  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.SQ_k4OCA.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.vMIeuOQq.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.XRz1-5H_.mjs').then(interopDefault)
-}

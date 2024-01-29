6b6669426a7588a457b36214017c4544c0bbb811
hjh805880 <hjh805880@gmail.com>
2024-01-29 10:43:28 +0900

    빌드 배포

---
+++ .nuxt/dist/server/_nuxt/entry-styles.SQ_k4OCA.mjs
@@ -0,0 +1,3 @@
+import style_0 from './entry-styles-1.mjs-TCZSiyzk.js';
+import style_1 from './entry-styles-2.mjs-DgoGqKIQ.js';
+export default [style_0, style_1]
---
+++ .nuxt/dist/server/_nuxt/island-renderer-n0g-odPM.js
@@ -0,0 +1,42 @@
+import { defineComponent, onErrorCaptured, createVNode } from "vue";
+import { c as createError } from "../server.mjs";
+import "#internal/nitro";
+import "ofetch";
+import "hookable";
+import "unctx";
+import "unhead";
+import "@unhead/shared";
+import "vue-router";
+import "h3";
+import "ufo";
+import "defu";
+import "klona";
+import "devalue";
+import "vue/server-renderer";
+import "aos";
+const islandComponents = {};
+const islandRenderer = defineComponent({
+  props: {
+    context: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const component = islandComponents[props.context.name];
+    if (!component) {
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Island component not found: ${props.context.name}`
+      });
+    }
+    onErrorCaptured((e) => {
+      console.log(e);
+    });
+    return () => createVNode(component || "span", { ...props.context.props, "data-island-uid": "" });
+  }
+});
+export {
+  islandRenderer as default
+};
+//# sourceMappingURL=island-renderer-n0g-odPM.js.map

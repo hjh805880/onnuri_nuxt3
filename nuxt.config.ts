e075aedb89910ee6d983e313aad0c4423c5275a4
hjh805880 <hjh805880@gmail.com>
2024-01-31 09:00:52 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   app: {
     head: {
+      htmlAttrs: { lang: "ko" },
       bodyAttrs: { class: "lg:max-w-[600px] mx-auto" },
       title: "온누리대부중개 | 무직자대출 소액대출 비대면대출",
 
@@ -24,12 +25,20 @@ export default defineNuxtConfig({
           content: "급한대출은 긴급대출, 무직자대출쉬운곳, 소액대출, 신용대출 한국대부중개협회 정식등록 업체 안심대출, 무방문,무서류,비대면대출",
         },
         {
+          name: "keywords",
+          content: "신용대출,여성주부,대출사업자,대출중고차,대출재대출,기대출과다자추가대출,소상공인대출,창업자대출,무서류대출,저신용자대출,군인대출,주부대출,법인대출,알바생대출,취준생대출,공무원대출,직장인대출",
+        },
+        {
           property: "og:title",
           content: "온누리대부중개 | 무직자대출 소액대출 비대면대출",
         },
         {
+          property: "og:description",
+          content: "급한대출은 긴급대출, 무직자대출쉬운곳, 소액대출, 신용대출 한국대부중개협회 정식등록 업체 안심대출, 무방문,무서류,비대면대출",
+        },
+        {
           property: "og:url",
-          content: "https://onnuriloan.com",
+          content: "https://onloan.kr",
         },
         {
           property: "og:image",
@@ -55,7 +64,7 @@ export default defineNuxtConfig({
 
       // 링크 설정
       link: [
-        { rel: "canonical", href: "https://onnuriloan.com" },
+        { rel: "canonical", href: "https://onloan.kr" },
         {
           rel: "stylesheet",
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css",

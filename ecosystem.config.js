c8688630c31688bdc94b82beb6df300326101831
hjh805880 <hjh805880@gmail.com>
2024-01-31 10:29:38 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-  apps: [{
-    name: 'onnuriloan',
-    cwd: '/home/bitnami/htdocs',
-    exec_mode: 'cluster',
-    instances: 'max',
-    script: './.output/server/index.mjs',
-    watch: ['./'],
-    watch_options: {
-      followSymlinks: true,
-      usePolling: true,
-      interval: 10000,
-      binaryInterval: 10000,
+  apps: [
+    {
+      name: "onnuriloan",
+      cwd: "/home/bitnami/htdocs",
+      script: "./.output/server/index.mjs",
+      exec_mode: "cluster",
+      instances: "max",
+      watch: false,
+      env: {
+        PORT: 3000,
+        NODE_ENV: "production",
+        // MariaDB_HOST: "localhost",
+        // MariaDB_PORT:3306,
+        // MariaDB_USER:"root",
+        // MariaDB_PASSWORD:"",
+        // MariaDB_NAME:"DB-Name",
+      },
     },
-    env: {
-      PORT: 3000,
-      NODE_ENV: 'production',
-    }
-  }]
-}
+  ],
+};
